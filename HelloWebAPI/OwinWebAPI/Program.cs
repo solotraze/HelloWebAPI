4fc9e78af7666e39e455edd08b16e3f3a91ac7f6
Kabir Sharan <Kabir.Sharan@condecosoftware.com>
2016-10-08 23:09:31 +0530

    Owin test app.

---
+++ HelloWebAPI/OwinWebAPI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace OwinWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                //HttpClient client = new HttpClient();
+
+                //var response = client.GetAsync(baseAddress + "api/knots").Result;
+
+                //Console.WriteLine(response);
+                //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ HelloWebAPI/OwinWebAPI/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace OwinWebAPI
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            config.MapHttpAttributeRoutes();
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
