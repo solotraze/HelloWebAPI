495f8125fd9d393f4bc92dfc88fbb89e375edb21
Solo Traze <traze.solo.dev2@gmail.com>
2016-10-10 20:33:07 +0530

    Knots Azure Mobile Service

---
+++ HelloWebAPI/KnotsMobileService/DataObjects/Knot.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KnotsMobileService.DataObjects
+{
+    public class Knot : EntityData
+    {
+        //public int KnotId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
