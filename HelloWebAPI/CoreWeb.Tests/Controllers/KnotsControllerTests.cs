bc2e3c005b28ae5ae2b241af975dd45ce0e7a5ce
Kabir Sharan <Kabir.Sharan@condecosoftware.com>
2016-10-27 00:46:04 +0530

    Added unit tests project. Deleted outdated mobile service project.

---
+++ HelloWebAPI/CoreWeb.Tests/Controllers/KnotsControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoreWeb.Tests.Controllers
+{
+    public class KnotsControllerTests
+    {
+        [Fact]
+        public void TestGet()
+        {
+            Assert.Equal(6, (3 + 3));
+        }
+    }
+}
--- HelloWebAPI/KnotsMobileService/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KnotsMobileService.WebApiApplication" Language="C#" %>
