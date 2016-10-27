147fb24d2859ff7387d2cfd76239900b63d38fc6
Kabir Sharan <Kabir.Sharan@condecosoftware.com>
2016-10-27 12:01:04 +0530

    Added basic unit test.

--- HelloWebAPI/CoreWeb.Tests/Controllers/KnotsControllerTests.cs
+++ HelloWebAPI/CoreWeb.Tests/Controllers/KnotsControllerTests.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using NSubstitute;
+using CoreWeb.Controllers;
+using KnotsInterfaces.BLL;
+using KnotsInterfaces.Repositories;
+using KnotsModels;
 
 namespace CoreWeb.Tests.Controllers
 {
     public class KnotsControllerTests
     {
+        public KnotsControllerTests()
+        {
+            Console.WriteLine("Inside constructor: KnotsControllerTests()");
+        }
+
         [Fact]
         public void TestGet()
         {
-            Assert.Equal(6, (3 + 3));
+
+            //var knotsRepoMock = Substitute.For<IKnotsRepo>();
+            var knotsBalMock = Substitute.For<IKnotsBAL>();
+            knotsBalMock.GetAllKnots().Returns(new List<Knot> {
+                                                new Knot { Title="Mock1", Content="Mock content1" } });
+            var controller = new KnotsController(knotsBalMock);
+            var result = controller.Get();
+            Assert.NotNull(result);
+            Assert.Equal(result.Count(), 1);
         }
     }
 }
