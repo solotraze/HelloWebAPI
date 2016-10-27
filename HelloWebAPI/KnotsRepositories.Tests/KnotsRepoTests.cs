fc344290ed4ba091404d3203cb435fcc35ed44f9
Kabir Sharan <Kabir.Sharan@condecosoftware.com>
2016-10-27 15:52:59 +0530

    Added Repo unit test project

---
+++ HelloWebAPI/KnotsRepositories.Tests/KnotsRepoTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+using NSubstitute;
+using KnotsInterfaces.Repositories;
+
+namespace KnotsRepositories.Tests
+{
+    public class KnotsRepoTests
+    {
+        public KnotsRepoTests()
+        {
+            Console.WriteLine("Inside constructor: KnotsRepoTests()");
+        }
+
+        [Fact]
+        public void GetAllKnotsTest()
+        {
+            var knotsRepo = new KnotsRepo();//Substitute.For<IKnotsRepo>();
+            var knots = knotsRepo.GetAllKnots();
+
+            Assert.NotNull(knots);
+            Assert.NotEmpty(knots); // 1 or more
+        }
+    }
+}
