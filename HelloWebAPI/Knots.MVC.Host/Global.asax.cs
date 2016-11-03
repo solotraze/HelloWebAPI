c7c1094391b0f68608b97b82fe8c668f7c7daa38
Solo Traze <traze.solo.dev2@gmail.com>
2016-11-03 23:50:25 +0530

    Autofac for MVC project

--- HelloWebAPI/Knots.MVC.Host/Global.asax.cs
+++ HelloWebAPI/Knots.MVC.Host/Global.asax.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Knots.Common.Interfaces.Repos;
+using Knots.Common.Interfaces.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -13,11 +18,37 @@ namespace Knots.MVC.Host
     {
         protected void Application_Start()
         {
+            /*****/
+            // http://docs.autofac.org/en/latest/integration/webapi.html
+
+            var builder = new ContainerBuilder();
+
+            // Get your HttpConfiguration.
+            var config = GlobalConfiguration.Configuration;
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            // OPTIONAL: Register the Autofac filter provider.
+            //builder.RegisterWebApiFilterProvider(config);
+
+            builder.RegisterType<Knots.Services.KnotsService>().As<IKnotsService>();
+            builder.RegisterType<Knots.Repos.KnotsRepo>().As<IKnotsRepo>();
+
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            /*****/
+
+
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            
+
         }
     }
 }
