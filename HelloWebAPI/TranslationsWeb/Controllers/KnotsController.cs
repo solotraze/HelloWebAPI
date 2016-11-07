282f56bea91af1a492a863b9f94ddff1076e4f7b
Kabir Sharan <Kabir.Sharan@condecosoftware.com>
2016-11-07 17:11:32 +0530

    A Core MVC project with localization texts

---
+++ HelloWebAPI/TranslationsWeb/Controllers/KnotsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TranslationsWeb.Controllers
+{
+    public class KnotsController : Controller
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public KnotsController(IStringLocalizer<KnotsController> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Greet()
+        {
+            return _localizer["Greetings"];
+        }
+    }
+}
