4507a99691cebe180e7bce2337f6d115df62a7e9
Kabir Sharan <Kabir.Sharan@condecosoftware.com>
2016-11-08 17:55:52 +0530

    More translation techniques like resiurce inside web-api, localizer factory and common resource files.

---
+++ HelloWebAPI/TranslationsWeb/CommonResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TranslationsWeb
+{
+    /// <summary>
+    /// Dummy class to make CommonResources.resx available
+    /// </summary>
+    class CommonResources
+    {
+    }
+}
